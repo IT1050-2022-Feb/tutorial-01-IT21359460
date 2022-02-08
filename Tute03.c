91da067abf7623378b20ac31407ce13f79132d21
IT21359460 <99164868+IT21359460@users.noreply.github.com>
2022-02-08 10:10:10 +0530

    Update Tute03.c

--- Tute03.c
+++ Tute03.c
@@ -8,12 +8,29 @@ e.g.
 n -> 100
 sum = 1+2+3+....+ 99+100 = 5050
 
-n -> 1-
+n -> 10
 sum = 1+2+3+...+10 = 55 */
+
 
 #include <stdio.h>
 int main() {
+  int num;
+  int sum = 0;
+  
+  printf("Enter number from 1 to n: ");
+  scanf("%d", &num );
+  
+  while( 1 <= num )
+        {
+          sum = sum + num;
+          num = num - 1; // num--
+          }
+  
+          printf("sum = %d\n", sum);
   
   return 0;
 }
+
+
+
 
